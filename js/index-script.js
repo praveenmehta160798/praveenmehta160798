8a358b736914314dedbee78c10732fdee8d695f1
Praveen Mehta <praveenmehta160798@gmail.com>
2022-11-24 20:16:39 +0530

    update but responsive pending

--- js/index-script.js
+++ js/index-script.js
@@ -1,41 +1,40 @@
-var hamBurger1 = document.querySelector("#hamburger-box .box1");
-var hamBurger2 = document.querySelector("#hamburger-box .box2");
-var hamBurger3 = document.querySelector("#hamburger-box .box3");
-var header = document.querySelector("#header");
-var hamBurgerBox = document.querySelector("#hamburger-box");
-hamBurgerBox.addEventListener("click", headerToggler);
-var toggle = 1;
-function headerToggler(){
-	if(toggle == 1){
-		header.style.left = "0%";
-		toggle = 0;
-		console.log("header show");
-		hamBurgerBox.style.left = "85px";
-		hamBurger1.style.background = "var(--theme-color-4)";
-		hamBurger2.style.background = "var(--theme-color-4)";
-		hamBurger3.style.background = "var(--theme-color-4)";
-		hamBurger1.style.transform = "rotate(45deg)";
-		hamBurger3.style.transform = "rotate(-45deg)";
-		hamBurger2.style.width = "0px";
+/*----------------------------------------------------------------------------------------*/
+
+/*Hamburger Start*/
+var hamburgerBox = document.querySelector("#header .hamburger-box");
+var hamburger1 = document.querySelector("#header .box1");
+var hamburger2 = document.querySelector("#header .box2");
+var hamburger3 = document.querySelector("#header .box3");
+var navBar = document.querySelector("#header .nav-bar");
+
+hamburgerBox.addEventListener("click", hamburgerToggler);
+var click = true;
+function hamburgerToggler(){
+	if(click){
+		navBar.style.right = "2%";
+		hamburger1.style.transform = "rotate(45deg)";
+		hamburger1.style.background = "var(--theme-color-1)";
+		hamburger3.style.transform = "rotate(-45deg)";
+		hamburger3.style.background = "var(--theme-color-1)";
+		hamburger2.style.width = "0px";
+		click = false;
 	}
 	else{
-		header.style.left = "-200%";
-		toggle = 1;
-		console.log("header hide");
-		hamBurgerBox.style.left = "45px";
-		hamBurger1.style.background = "var(--theme-color-3)";
-		hamBurger2.style.background = "var(--theme-color-3)";
-		hamBurger3.style.background = "var(--theme-color-3)";
-		hamBurger1.style.transform = "rotate(0deg)";
-		hamBurger3.style.transform = "rotate(0deg)";
-		hamBurger2.style.width = "30px";
-		
+		navBar.style.right = "-100%";
+		hamburger1.style.transform = "rotate(0deg)";
+		hamburger1.style.background = "var(--theme-color-4)";
+		hamburger3.style.transform = "rotate(0deg)";
+		hamburger3.style.background = "var(--theme-color-4)";
+		hamburger2.style.width = "100%";
+		click = true;
 	}
 }
+/*Hamburger End*/
 
+/*----------------------------------------------------------------------------------------*/
 
-
-
+/*Auto Type Start*/
+/*
 var keywords = [" Programmer", " Designer", " Developer"];
 var word = document.querySelector(".auto-type");
 var hide = 1, j = 0, k = 0;
@@ -72,4 +71,57 @@ function type(){
 			j = j + 1;
 		}	
 	}
+}*/
+/*------------*/
+var keyword = ["Designer", "Developer", "Programmer"];
+//var keyword = "Programmer";
+var editWord, len;
+setInterval(autoType, 300);
+var i = len, j = 1, k = 0;
+function autoType(){
+	len = keyword[k].length;
+	if(i == 0){
+		k = k + 1;
+	}
+	if(k > 2){
+		k = 0;
+	}
+	if(i > -1){
+		editWord = keyword[k].substr(0,i);
+		document.querySelector("#home .auto-type").innerText = editWord;
+		//console.log(editWord);
+		//console.log(i);
+		i--;
+		j = 1;
+	}
+	else{
+		if(j < len){			
+			editWord = keyword[k].substr(0, j);
+			document.querySelector("#home .auto-type").innerText = editWord;
+			//console.log(editWord);
+			//console.log(j);
+			j++;
+		}
+		else{
+			i = len;
+		}
+	}
+	
 }
+
+
+/*
+var len = keyword.length;
+for(var i = len; i > -2; i--){
+	if(i == -1){
+		for(var j = 1;j < len + 1; j++){			
+			backup = keyword.substr(0,j);
+			console.log(backup);
+		}
+	}
+	else{
+		backup = keyword.substr(0,i);
+		console.log(backup);
+	}
+}
+/*Auto Type End*/
